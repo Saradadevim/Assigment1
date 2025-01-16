128262517531cc18a164932f971abf5f9612d79a
Saradadevim <saradakukkuu15@gmail.com>
2025-01-16 10:25:16 +0530

    Assignment1-4D

---
+++ Assignment1-4D.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment1_4D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int y = -5; y <= 5; y++)
+            {
+                int x = (y * y) + (2 * y) + 1;
+                Console.WriteLine($"y = {y}, x = {x}");
+            }
+        }
+    }
+}
+     
+//display certain values of a function x=y*y+2y+1
+//using numbes for y from -5 to 5
